2349b1695379625c1c65400cd21668fbe0356882
Aytaj Kareem <kareemaytaj@gmail.com>
2021-05-22 00:52:13 -0400

    Added more hello features

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developed Hello Feature");
+        System.out.println("Local repo is already published to Github");
+        System.out.println("Doing more work");
     }
 }
